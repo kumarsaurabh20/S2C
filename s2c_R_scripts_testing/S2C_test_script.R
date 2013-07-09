d870c6e7b67fb90698c5a557393f5af3c6b2c9bf
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2013-07-09 20:31:07 +0200

    update

--- s2c_R_scripts_testing/S2C_test_script.R
+++ s2c_R_scripts_testing/S2C_test_script.R
@@ -10,7 +10,7 @@ numOfRows <- length(y)
 frame1 <- data.frame(d0=rep(1,each=length(y)), d1=y)
 mX <- data.matrix(frame1)
 theta = data.matrix(data.frame(theta0=0, theta1=0))
-plot(x, y)
+histSEF <- rep(0, each=num_iters)
 
     computeLineOfFit <- function(mX, y, theta) {
       
@@ -30,8 +30,6 @@ plot(x, y)
     gradDescentUniVar <- function(mX,y,theta,alpha,num_iters) {
       
       m <- length(y)
-      
-      histSEF <- rep(0, each=num_iters)
       #histSEF <- t(t(histSEF))
       #thetaHistory <- rep(0, each=num_iters)
       
@@ -42,24 +40,28 @@ plot(x, y)
           for(i in 1:m) {
           
             init <- init + (alpha/m) * (mX[i,] %*% t(theta) - y[i]) * mX[i,]
-            print(init)
-                        
           }
-       
       theta = theta - init
       #print(theta)   
-      
       histSEF[iter] <- computeLineOfFit(mX,y,theta)   
-      #thetaHistory[iter] <- theta
-        
-      plot(1:num_iters, histSEF, xlab="Number of Iterations", ylab="Minimized Squared error function", main="Gradient Descent Check")  
-        
+      #thetaHistory[iter] <- theta        
       }
       
-      return(theta)
-      
+      plot(1:num_iters, histSEF, xlab="Number of Iterations", ylab="Minimized Squared error function", main="Gradient Descent Check")  
+      output <- list(thetaVal=theta, J=histSEF)
+      return(output)
     }
-
- gradDescentUniVar(mX,y,theta,alpha,num_iters)
-
+ result <- gradDescentUniVar(mX,y,theta,alpha,num_iters) 
+ return(result)
 }
+
+#Run the script as below to retrieve the values
+a <- UniVarLinReg("array_for_R.txt", 0.01,150)
+b <- unlist(a)
+b[1]
+b[2]
+b[3]
+...
+
+
+
