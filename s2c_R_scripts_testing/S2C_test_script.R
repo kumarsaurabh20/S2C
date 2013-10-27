e3f2110349c4edf12f82e7e2524fe3b17f948daf
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2013-10-27 02:31:58 +0200

    update

--- s2c_R_scripts_testing/S2C_test_script.R
+++ s2c_R_scripts_testing/S2C_test_script.R
@@ -3,13 +3,14 @@ UniVarLinReg <- function(file, alpha, num_iters) {
 file <- as.character(file)
 alpha <- as.numeric(alpha)
 num_iters <- as.numeric(num_iters)
-calibData <- read.table(file)
+calibData <- read.csv(file)
 x <- as.numeric(calibData$X)
 y <- as.numeric(calibData$Y)
 numOfRows <- length(y)
 frame1 <- data.frame(d0=rep(1,each=length(y)), d1=y)
 mX <- data.matrix(frame1)
-theta = data.matrix(data.frame(theta0=0, theta1=0))
+theta = data.matrix(data.frame( theta0=0, theta1=0 ))
+thetaRep = data.matrix(data.frame(theta0=rep(0, num_iters), theta1=rep(0, num_iters)))
 histSEF <- rep(0, each=num_iters)
 
     computeLineOfFit <- function(mX, y, theta) {
@@ -40,15 +41,17 @@ histSEF <- rep(0, each=num_iters)
           for(i in 1:m) {
           
             init <- init + (alpha/m) * (mX[i,] %*% t(theta) - y[i]) * mX[i,]
+      
           }
       theta = theta - init
+      thetaRep[iter,] = theta
       #print(theta)   
       histSEF[iter] <- computeLineOfFit(mX,y,theta)   
       #thetaHistory[iter] <- theta        
       }
       
       plot(1:num_iters, histSEF, xlab="Number of Iterations", ylab="Minimized Squared error function", main="Gradient Descent Check")  
-      output <- list(thetaVal=theta, J=histSEF)
+      output <- list(thetaOriVal=theta, thetaAll=thetaRep, J=histSEF)
       return(output)
     }
  result <- gradDescentUniVar(mX,y,theta,alpha,num_iters) 
@@ -69,5 +72,9 @@ histSEF <- rep(0, each=num_iters)
 ## Tukey-Anscombe Plot :
 #plot(residuals(z) ~ fitted(z), main = deparse(z$call))
 
-
-
+#a[[2]]
+#theta.values <- a[[2]]
+#theta.values.frame <- data.frame(theta.values)
+#theta0Values <- theta.values.frame$theta0
+#theta1Values <- theta.values.frame$theta1
+#a[[2]] <- NULL
