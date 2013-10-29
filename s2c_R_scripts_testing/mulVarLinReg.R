a2787c5f2e0f58616f5f64cd64e33f00572e101c
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2013-10-29 11:58:17 +0100

    update

--- s2c_R_scripts_testing/mulVarLinReg.R
+++ s2c_R_scripts_testing/mulVarLinReg.R
@@ -7,18 +7,36 @@ num_iters <- as.numeric(num_iters)
 calibMultiData <- read.csv(file)
 x1 <- as.numeric(calibMultiData$X1)
 x2 <- as.numeric(calibMultiData$X2)
+x3 <- as.numeric(calibMultiData$X3)
 y <- as.numeric(calibMultiData$Y)
-mX <- cbind(x1,x2)
+#mX <- cbind(x1,x2)
 numOfRows <- length(y)
+theta = t(data.matrix(data.frame(theta0=0, theta1=0, theta2=0))) 
+thetaRep = data.matrix(data.frame(theta0=rep(0, num_iters), theta1=rep(0, num_iters), theta2=rep(0, num_iters)))
 
-mean <- cbind(mean(mX[,1]), mean(mX[,2]))
-sd <- cbind(sd(mX[,1]), sd(mX[,2]))
+if(length(x3) == 0) { 
+  mX <- cbind(x1,x2)
+  mean <- cbind(mean(mX[,1]), mean(mX[,2]))
+  sd <- cbind(sd(mX[,1]), sd(mX[,2]))
+  theta = t(data.matrix(data.frame(theta0=0, theta1=0, theta2=0))) 
+  thetaRep = data.matrix(data.frame(theta0=rep(0, num_iters), theta1=rep(0, num_iters), theta2=rep(0, num_iters)))
+} else {
+  mX <- cbind(x1,x2,x3)
+  mean <- cbind(mean(mX[,1]), mean(mX[,2]), mean(mX[,3]))
+  sd <- cbind(sd(mX[,1]), sd(mX[,2]), sd(mX[,3]))
+  theta = t(data.matrix(data.frame(theta0=0, theta1=0, theta2=0, theta3=0)))
+  thetaRep = data.matrix(data.frame(theta0=rep(0, num_iters), theta1=rep(0, num_iters), theta2=rep(0, num_iters), theta3=rep(0, num_iters)))
+}
+
+
+#mean <- cbind(mean(mX[,1]), mean(mX[,2]))
+#sd <- cbind(sd(mX[,1]), sd(mX[,2]))
 con <- rep(1, length(y))
 mX <- (mX - (con %*% mean))/(con %*% sd)
 cX <- cbind(x0=rep(1,each=length(y)), mX)
 
 
-theta = t(data.matrix(data.frame(theta0=0, theta1=0, theta2=0)))
+#theta = t(data.matrix(data.frame(theta0=0, theta1=0, theta2=0)))
 
 
     SEF <- function(cX, y, theta) {  
@@ -34,15 +52,17 @@ theta = t(data.matrix(data.frame(theta0=0, theta1=0, theta2=0)))
        for(iter in 1:num_iters) {
          
          theta = theta - (alpha/m) * t(t((cX %*% theta) - y) %*% cX)
+         thetaRep[iter,] = theta
          histSEF[iter] <- SEF(cX,y,theta)
-         print(histSEF)
+        
          plot(1:num_iters, histSEF, xlab="Number of Iterations", ylab="Minimized Squared error function", main="Gradient Descent Check") 
        }
-  
-    return(theta)
+    output <- list(thetaOriVal=theta, thetaAll=thetaRep, J=histSEF) 
+    return(output)
     
     }
 
- multiGradDesc(cX,y,theta,alpha,num_iters)
+ result <- multiGradDesc(cX,y,theta,alpha,num_iters)
+ return(result)
 
 }
