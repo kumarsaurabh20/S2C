4e504698fe420fdaed45148612eebee0e9ed2c35
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2014-01-04 22:38:45 +0100

    update

--- s2c_R_scripts_testing/getFitCoefs.R
+++ s2c_R_scripts_testing/getFitCoefs.R
@@ -7,17 +7,25 @@
 #######################################################################################################
 
 
-getFitCoefs <- function(probe, columns, cells) {
 
-columns <- matrix(0, length(probes), count)
-
-     for (i in c(1:count)) {
+#columns can be built by combining individual columns read from raw calibration file
+for (i in c(1:count)) {
          if (i == 1) { columns <- cbind(get(paste0("col",i))) } 
          else { columns <- cbind(columns, get(paste0("col",i))) }    
          }
+
+
+#norm_probes are probes for normalization, cells are the cell count for every serial analysis. Cells values
+#should be available in the calibration file. Count is the number of columns in the calibration file/columns
+getFitCoefs <- function(norm_probes, columns, cells, count) {
+
+columns <- matrix(0, length(probes), count)
+
      
+        #create a matrix for carrying normalization probe signal intensities for every serial analysis
 	norm_val <- matrix(0, length(norm_probes), ncol(columns) - 1)
 
+        #fill the norm_val matrix
         for (i in 1:length(norm_probes)) {
         dummy <- columns[norm_probes[i] == columns[,1]]
         print(dummy)
@@ -25,7 +33,10 @@ columns <- matrix(0, length(probes), count)
         norm_val[i,] <- dummy
         }
 
+  #filter the first column i.e probe names
   column_filter <- columns[, -1]
+
+  #create some variable and empty matrices
   col <- ncol(column_filter)
   row <- nrow(column_filter)
   tab_norm_1 <- matrix(0, row,col)
@@ -43,6 +54,12 @@ columns <- matrix(0, length(probes), count)
   myData <- list()
 
 
+#Normalize each column of serial analysis by norm_probes values i.e divide signal intensities of each probes
+#in the calibration file by normalization probe intensities. Then take a transpose of that matrix to have serial values of each probe in one column. then preapare a matrix of cell number normalized values from individual normalization probe and put it in a list variable.
+
+#every if statement corresponds to number of normalization probes.
+
+#Maximum of 6 normalization probes allowed, pertaining to 6 different steps of microarray experiment.
   if (length(norm_probes) == 2) {
 	for(i in c(1:ncol(norm_val))) {tab_norm_1[,i] <- unlist(lapply(as.numeric(column_filter[,i]), function(x) {x/as.numeric(norm_val[1,i])}))}
 	for(i in c(1:ncol(norm_val))) {tab_norm_2[,i] <- unlist(lapply(as.numeric(column_filter[,i]), function(x) {x/as.numeric(norm_val[2,i])}))}
@@ -99,7 +116,8 @@ for (i in c(1:ncol(t_tab_norm_1))) {myData[[i]] <- cbind(cells, t_tab_norm_1[,i]
 }
 
 
-
+#function for performing linear multiple regression and extracting regression coeffecients
+#so depending on number of normalization probes utilized the regression fit will be calculated.
 calLinMod <- function(x) {
             x <- as.matrix(x)
             if (ncol(x) == 2) {fit <- lm(x[,1] ~ x[,2])}
@@ -111,14 +129,20 @@ calLinMod <- function(x) {
             return(as.numeric(coef(fit)))
        }
 
+ #call the regression function
  fitted_coeffs <- sapply(myData, calLinMod)
+ #create a matrix of fitted coeffecients.
  coeffs_matrix <- matrix(fitted_coeffs, nrow(columns), length(norm_probes) + 1, byrow = T)
+
+ #create a list of all probe list
  probe_list <- columns[, 1]
+
+ #the original probe list must be merged with their coeffecients.
  result_matrix <- cbind(probe_list, coeffs_matrix)
 
+ #return the final resultant matrix
  return(result_matrix)
 }
-
 
 
 
