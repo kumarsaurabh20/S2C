5b503080e0ad33ac374736cd9e9ba3fe4655bb01
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2014-04-14 03:07:36 +0200

    update

--- s2c_R_scripts_testing/s2c_normalization_script.R
+++ s2c_R_scripts_testing/s2c_normalization_script.R
@@ -1,11 +1,46 @@
+##passing multiple arguments without knowing number of arguments
+#int1 <- 3
+#int2 <- 5
+#do.call(sum, lapply(ls(pat="^int\\d$"), get))
+## [1] 8
+## Or use a safer version that will only look for objects located in the 
+## environment from which it was called.  
+#do.call(sum, lapply(ls(pat="^int\\d$"), get, envir=parent.frame(), inherits=FALSE))
+##OR
+## sum those arguments whose names match ^int\\d$
+#sumint <- function(...) {
+#  arg.names <- as.character(match.call()[-1])
+#  ix <- grep("^int\\d$", arg.names)
+#  do.call("sum", list(...)[ix])}
+## test    
+#a <- b <- 10
+#int3 <- 30
+#sumint(a, int3, b, int3, b, pi) # 60
+##=============================================
+
+
+
+getCoefForNorm <- function(probe, col_1, col_2, col_3, col_4) {
+  
+  
+  
+  
+  
+}
+
 # make sure to convert them in character and numeric vectors
 probe <- c('POSITIVE_25_dT','Poly-T-CY5')
-col_1 <- c('POSITIVE_25_dT', 'Poly-T-CY5', 'EukS_1209_25_dT', 'Test15 (EukS_1209_25dT)','EukS_328_25_dT')
-col_2 <- c(7.55E+07, 4.51E+08, 4102788.91290624, 3242670.65825, 4564828.4446875)
-col_3 <- c(5.73E+07, 3.97E+08, 1.68E+07, 1.99E+07, 2.18E+07)
-col_4 <- c(1.01E+08, 2.39E+08, 2.62E+08, 3.92E+08, 4.40E+08)
+col1 <- c('POSITIVE_25_dT', 'Poly-T-CY5', 'EukS_1209_25_dT', 'Test15 (EukS_1209_25dT)','EukS_328_25_dT')
+col2 <- c(7.55E+07, 4.51E+08, 4102788.91290624, 3242670.65825, 4564828.4446875)
+col3 <- c(5.73E+07, 3.97E+08, 1.68E+07, 1.99E+07, 2.18E+07)
+col4 <- c(1.01E+08, 2.39E+08, 2.62E+08, 3.92E+08, 4.40E+08)
+col5 <- c(8.87E+07, 3.41E+08, 5.41E+08, 3.73E+08, 6.77E+08)
 
-table <- data.frame(col_1,col_2,col_3,col_4,col_5)
+#prepare a function and call with list of arguments for variables
+#do.call(sum, lapply(ls(pat="^pol\\d$"), get, envir=parent.frame(), inherits=FALSE))
+list <- ls(pat="^col\\d$")
+
+table <- data.frame(list)
 #match the selected probe in the table
 newData <- subset(table, col_1 == probe[1], select=c(col_2, col_3, col_4, col_5))
 newData2 <- subset(table, col_1 == probe[2], select=c(col_2, col_3, col_4, col_5))
